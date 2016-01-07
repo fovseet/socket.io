5e8a0b2898a93c19724d84f4c51a38ef7d1bd0b0
fovsweet <619532786@qq.com>
2016-01-07 23:58:54 +0800

    test1.0.1

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('./server/node_modules/socket.io')(http);
 
-app.set("port",3000);
+app.set("port",4555);
 
 /*使用模块*/
 app.use(logger("dev"));
